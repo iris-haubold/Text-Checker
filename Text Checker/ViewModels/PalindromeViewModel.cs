758f4c89aa9ec883c3271c3ba5b40a6446a2406e
Iris Haubold <e11810907@student.tuwien.ac.at>
2023-10-14 18:47:58 +0200

    Add project files.

---
+++ Text Checker/ViewModels/PalindromeViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Text_Checker.ViewModels
+{
+    public class PalindromeViewModel : Screen
+    {
+        private string palindromes;
+
+        public PalindromeViewModel(string textInput) 
+        {
+            palindromes = FindPalindromes(textInput, "");
+            if (palindromes == "")
+            {
+                palindromes = "Keine Palindrome gefunden :/";
+            }
+        }
+         
+        public string Palindromes
+        {
+            get { return palindromes; }
+            set { palindromes = value; }
+        }
+
+        // discovers all palindromes included in the text
+        // only checks strings separated by spaces
+        public string FindPalindromes(string text, string palindromes)
+        {
+            //separate text into words by looking for spaces
+            //for each word: check if it is a palindrome
+            //if it is a palindrome, add it to the palindromes string
+
+            int firstSpace = text.IndexOf(' ');
+           
+            string nextWord = firstSpace == -1? text : text.Substring(0, firstSpace);
+            palindromes += IsPalindrome(nextWord) ? nextWord + " " : "";
+
+            return firstSpace == -1 ? palindromes : FindPalindromes(text.Substring(firstSpace + 1), palindromes);
+
+        }
+
+        // checks whether word is a palindrome in the sense that is the same forwards as backwards
+        // does not check for palindromes like "Regal - Lager"
+        // also does not check whether the word makes sense
+        public bool IsPalindrome(string word)
+        {
+            // if word contains chars other than letters or is empty, it isn't a palindrome
+            if (!word.All(Char.IsLetter) || word == "")
+            {
+                return false;
+            }
+
+            word = word.ToLower();
+            string wordReversed = Reverse(word);
+            
+            return word.Equals(wordReversed);
+        }
+
+        // reverses a string so it's backwards
+        public string Reverse(string word)
+        {
+            char[] tempArray = word.ToCharArray();
+            Array.Reverse(tempArray);
+            return new string(tempArray);
+        }
+
+    }
+
+
+   
+}
---
+++ Text Checker/ViewModels/ShellViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Checker.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+		private string _textInput;
+
+		public string TextInput
+		{
+			get { return _textInput; }
+			set 
+			{ 
+				_textInput = value;
+				NotifyOfPropertyChange(() => TextInput);
+			}
+		}
+
+		// true if text input is not empty
+		public bool CanClearText(string textInput)
+		{
+			return !string.IsNullOrWhiteSpace(textInput);
+		}
+
+		// clears the input text
+		public void ClearText(string textInput)
+		{
+			TextInput = string.Empty;
+		}
+
+		public void LoadPalindromePage()
+		{
+			ActivateItemAsync(new PalindromeViewModel(TextInput));
+		}
+
+	}
+}
